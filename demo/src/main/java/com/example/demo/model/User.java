8f23884851ac1dff0ada6b27a4b3ee24ac4250c3
kurumaddali-s <sushmitha7598@gmail.com>
2025-10-16 07:37:57 -0400

    second commit

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;  
-public class User {
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;  
 
+@Entity
+@Table(name = "users")
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
